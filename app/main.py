58ab57196cf47b82bd49b66c0656e8d120dd6627
Danylo Tatarchuk <tatarchuk.danil06@gmail.com>
2025-08-20 11:33:11 +0300

    fix newline and character

--- app/main.py
+++ app/main.py
@@ -7,17 +7,19 @@ class Person:
         Person.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
-    result_list = []
+def create_person_list(people: list[dict]) -> list:
+    Person.people.clear()
 
-    for person_data in people:
-        person = Person(person_data["name"], person_data["age"])
-        result_list.append(person)
+    result_list = [Person(p["name"], p["age"]) for p in people]
 
     for person_data in people:
         person = Person.people[person_data["name"]]
-        if "wife" in person_data and person_data["wife"]:
-            person.wife = Person.people.get(person_data["wife"])
-        if "husband" in person_data and person_data["husband"]:
-            person.husband = Person.people.get(person_data["husband"])
+
+        wife_name = person_data.get("wife")
+        if wife_name:
+            person.wife = Person.people.get(wife_name)
+
+        husband_name = person_data.get("husband")
+        if husband_name:
+            person.husband = Person.people.get(husband_name)
     return result_list
